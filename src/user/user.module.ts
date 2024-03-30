d877b4a92250461fc474fba9cffb3684c8c283d9
Volodya Andrushevskyi <xxvolodyax7@gmail.com>
2024-03-30 11:38:18 +0200

    auth: made login controller

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from 'src/prisma.service';
+import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, PrismaService],
+  providers: [PrismaService, UserService],
+  exports: [UserService]
 })
 export class UserModule {}
