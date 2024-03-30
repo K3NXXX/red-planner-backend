cbf10c3bb94d3c88ff879099d682a9e87e9875a3
Volodya Andrushevskyi <xxvolodyax7@gmail.com>
2024-03-30 13:28:03 +0200

    auth: made register logic

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { hash } from 'argon2';
+import { AuthDto } from 'src/auth/dto/auth.dto';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UserService {
 	constructor(private prisma: PrismaService) {}
+
 	async getById(id:string) {
 		return this.prisma.user.findUnique({
 			where: {
@@ -14,6 +17,7 @@ export class UserService {
 			}
 		})
 	}
+
 	async getByEmail(email:string) {
 		return this.prisma.user.findUnique({
 			where: {
@@ -21,4 +25,17 @@ export class UserService {
 			}
 		})
 	}
+
+	async create(dto: AuthDto) {
+		const user = {
+			email: dto.email,
+			name: '',
+			password: await hash(dto.password)
+		}
+		return this.prisma.user.create({
+			data: user
+		})
+		
+	}
+
 }
