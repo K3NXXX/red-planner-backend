dc3fbe8ea201358b53bc48148fcac3aaa44e79d9
Volodya Andrushevskyi <xxvolodyax7@gmail.com>
2024-04-01 20:53:06 +0300

    pomodoro-timer: created controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from './task/task.module';
 import { TimeBlockModule } from './time-block/time-block.module';
+import { PomodoroModule } from './pomodoro/pomodoro.module';
 
 @Module({
-	imports: [AuthModule, TimeBlockModule, TaskModule, UserModule, ConfigModule.forRoot()]
+	imports: [AuthModule, TimeBlockModule, PomodoroModule, TaskModule, UserModule, ConfigModule.forRoot()]
 })
 export class AppModule {}
