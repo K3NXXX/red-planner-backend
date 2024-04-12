8c5adbe55c0f7cc2499b4ea2bb92f1c836a009fa
Volodya Andrushevskyi <xxvolodyax7@gmail.com>
2024-04-12 14:54:42 +0300

    cors: changed allowed domains

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 	app.setGlobalPrefix('api');
 	app.use(cookieParser());
 	app.enableCors({
-		origin: ['http://localhost:3000'],
+		origin: ['https://red-planner-frontend.vercel.app'],
 		credentials: true,
 		exposedHeaders: 'set-cookie'
 	});
