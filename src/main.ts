d068ddb39cf14cd1e13d3ef7e6bafaeb6ed443cf
Volodya Andrushevskyi <xxvolodyax7@gmail.com>
2024-04-12 12:37:59 +0300

    vercel: changed main url

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	app.setGlobalPrefix('api');
 	app.use(cookieParser());
 	app.enableCors({
-		origin: ['http://localhost:3000'],
+		origin: ['https://red-planner-backend-986b30jx6-k3nxs-projects.vercel.app'],
 		credentials: true,
 		exposedHeaders: 'set-cookie'
 	});
